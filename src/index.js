d45499fb9082af6e967ad138f0bf185a9b5ff31b
RyanZhu <troyzx1024@gmail.com>
2016-03-11 20:11:55 -0800

    add jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY='AIzaSyC8xtYgXmn8l-j3xzUKt_p4ZAiwThiihw8';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//const ES6, it's a constant, never change after created
+const App = () => {
+    return <div>Hi!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />,document.querySelector('.container'));
